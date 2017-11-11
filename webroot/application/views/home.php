59c9c88ef10c3ad9fa23a859f79633498889c59c
Piet Voute <pvoute@sfu.ca>
2017-11-10 19:56:35 -0800

    Move the sample navbar from the home page to the header partial

--- webroot/application/views/home.php
+++ webroot/application/views/home.php
@@ -1,28 +1,3 @@
-<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-primary">
-	<a class="navbar-brand" href="#">Rare Eats</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="navbarText">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">My Playlists</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Recommended</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="<?php echo site_url('restaurants/create'); ?>">Add a 	Restaurant</a>
-			</li>
-		</ul>
-		<span class="navbar-text">
-			Eat how you feel
-		</span>
-	</div>
-</nav>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col">
