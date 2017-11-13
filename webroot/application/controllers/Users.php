f5ba1b864cdd9c8928fb6e2ebc4998facc58c6af
Farzin Ahmed <farzin94mimmi@gmail.com>
2017-11-13 12:23:14 -0800

    Remove redundant check in search function
    - I was checking if input->post existed or not and returning "users not
      found" but this is unncessary because I am already printing "users not
      found" in the view when $users is empty

--- webroot/application/controllers/Users.php
+++ webroot/application/controllers/Users.php
@@ -16,9 +16,8 @@ class Users extends CI_Controller {
         $return = [];
         if ($results) {
             $return['users'] = $this->users_model->get_users_by_email($results);
-        } else {
-            $return = 'User not found';
         }
+
         $this->load->view('partials/header');
         $this->load->view('users/search', $return);
         $this->load->view('partials/footer');
