0b67cb2176ad8b5d6b27d48b34e8764abbd2ff0f
Piet Voute <pvoute@sfu.ca>
2017-11-13 10:32:28 -0800

    Added functionality in header and footer to load all style sheets and javascripts passed to them through data. Add an array of file names (minus the css and js extensions) to the 'javascript' or 'css' keys of the data array, and they will be loaded by the header and footer.

--- webroot/application/views/partials/header.php
+++ webroot/application/views/partials/header.php
@@ -9,5 +9,12 @@
 
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+	<!-- Load additional CSS files passed in $data, should they exist -->
+	<?php if(isset($css)): ?>
+		<?php foreach($css as $style): ?>
+			<link rel="stylesheet" href="<?php echo $style.'.css'; ?>"></script>
+		<?php endforeach; ?>
+	<?php endif; ?>
   </head>
   <body>
