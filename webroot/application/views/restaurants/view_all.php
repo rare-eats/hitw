f13a174bc3ce462db95f6219f6222e0171fa674f
Mircea Taras <mtaras@sfu.ca>
2017-11-15 18:18:55 -0800

    Added 4 random restaurants to homepage, function supports other amounts of restaurants if needed, restaurants list view available at localhost:8080/restaurants.

--- webroot/application/views/restaurants/view_all.php
+++ webroot/application/views/restaurants/view_all.php
@@ -1,4 +1,9 @@
 <div class="container">
+<?php if(empty($restaurant)) 
+{
+	echo 'No restaurants available.';
+}
+?>
 <?php foreach ($restaurants as $restaurant): ?>
 	<a style="text-decoration: none;" href="<?php echo site_url('restaurants/'.$restaurant['id']); ?>">
 		<div class="card" style="margin-bottom: 1rem; padding: 1rem;">
