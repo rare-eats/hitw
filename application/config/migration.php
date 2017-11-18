c2d4acfd2ab5ab051849c9e7a4bc8c5a3063c05c
isis <isisl@sfu.ca>
2017-11-18 07:18:46 +0000

    updated migration file

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20171108130811;
+$config['migration_version'] = 20171117111600;
 
 /*
 |--------------------------------------------------------------------------
