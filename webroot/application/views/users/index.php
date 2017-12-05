0752a2508bb8f24288e93fe553571c24c5eac339
Farzin Ahmed <farzin94mimmi@gmail.com>
2017-12-04 20:42:51 -0800

    Make users list admin only
    - Index page showing list of users should only be visible to admins or
      devs who are debugging

--- webroot/application/views/users/index.php
+++ webroot/application/views/users/index.php
@@ -1,23 +1,27 @@
-<div class="container">
-    <div class="card">
-        <div class="card-body"> 
-    <h3><?php echo $title; ?></h3>
-<table class="table">
-    <tr>
-        <th>ID</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>email</th>
-    </tr>
-    <?php foreach($users as $user): ?>
-    <tr>
-        <td><?php echo $user['id']; ?></td>
-        <td><?php echo $user['first_name']; ?></td>
-        <td><?php echo $user['last_name']; ?></td>
-        <td><?php echo $user['email']; ?></td>
-    </tr>
-    <?php endforeach; ?>
-</table>
+<?php if($this->users_model->is_admin()): ?>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+        <h3><?php echo $title; ?></h3>
+    <table class="table">
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>email</th>
+        </tr>
+        <?php foreach($users as $user): ?>
+        <tr>
+            <td><?php echo $user['id']; ?></td>
+            <td><?php echo $user['first_name']; ?></td>
+            <td><?php echo $user['last_name']; ?></td>
+            <td><?php echo $user['email']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+            </div>
         </div>
     </div>
-</div>
+<?php else: ?>
+    <h1>You do not have permissions to view this page</h1>
+<?php endif ?>
